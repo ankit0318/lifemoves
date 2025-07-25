5b6114b3fcd16f2cf70e3da340d92e4e2cf39c4b
Ankit sharma <ankitsharma0318@gmail.com>
2025-07-25 17:46:37 +0530

    Fix hydration errors by adding 'use client' directives

--- src/components/Somatic/OfferSection.tsx
+++ src/components/Somatic/OfferSection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState } from "react";
 import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
 import TimerIcon from '@mui/icons-material/Timer';
